d56828e838833ae357aa2d1eb73fd36644030e38
Umarkhon Akhmedov <umarkhonakhmedov2000@gmail.com>
2025-01-29 16:14:21 +0500

    Creating next app - initial steps

---
+++ app/layout.js
@@ -0,0 +1,29 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Manage Finance",
+  description: "Amazing Finance Web App",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>Copyright © Umarkhon</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
